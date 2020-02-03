80e46cc4231ea3713c185ecd08aad605a705a0ae
Lohan Ollivier <lohanollivier@gmail.com>
2020-02-03 10:16:36 +0100

    Exploration
    Exploration de L'empire Humain

--- Explo.c
+++ Explo.c
@@ -1,4 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+char foret[15]= "foret";
+char montagne[15]= "montagne";
+int dec = 0;
+int res;
 
 int main(void)
+{
+	printf("Vous etes un soldat de l'Empire Humain\n");
+	printf("Vous etes a la recherche d'un homme tres important\n");
+	printf("Quel sera votre premier lieu d'investigation ?\n");
+	printf("La foret de Grauwald ?\n");
+	printf("Les montagnes de Hokberg ?\n");
+	res = scanf("%d", &dec);
+  if (dec == 1)
+  {
+    printf("Vous arrivez dans la foret de Grauwald\n");
+    printf("Vous sentez une vague odeur de souffre\n");
+  }
+  if (dec == 2) 
+  {
+    printf("Vous arrivez dans les montagnes de Hokberg\n");
+    printf("Vous sentez une vague odeur de poudre");
+  }
+
+}
